e2f5089d8302e3da9e0ca0cbe2abae4c04aff727
862190424@qq.com <862190424@qq.com>
2026-06-04 11:30:06 +0800

    Increase the thread pool

--- src/thread/threadpool.cpp
+++ src/thread/threadpool.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "threadpool.h"
+
+ThreadPool::ThreadPool(size_t thread_count) {
+    workers_.reserve(thread_count);
+    for (size_t i = 0; i < thread_count; ++i) {
+        workers_.emplace_back([this]() { workerLoop(); });
+    }
+}
+
+ThreadPool::~ThreadPool() {
+    mStop();
+}
+
+void ThreadPool::mStop() { {
+        std::lock_guard<std::mutex> lock_guard(mutex_);
+        stop_ = true;
+    }
+
+    cv_.notify_all();
+    for (auto &worker: workers_) {
+        if (worker.joinable()) {
+            worker.joinable();
+        }
+    }
+}
+
+void ThreadPool::submit(Task task) { {
+        std::lock_guard<std::mutex> lock_guard(mutex_);
+        tasks_.push(std::move(task));
+    }
+    cv_.notify_one();
+}
+
+void ThreadPool::workerLoop() {
+    while (true) {
+        Task task; {
+            std::unique_lock<std::mutex> lock(mutex_);
+            cv_.wait(lock, [this] {
+                return stop_ || tasks_.empty();
+            });
+
+            if (stop_ && tasks_.empty()) {
+                return;
+            }
+
+            task = std::move(tasks_.front());
+            tasks_.pop();
+        }
+        task();
+    }
+}
--- src/thread/threadpool.h
+++ src/thread/threadpool.h
@@ -5,8 +5,33 @@
 #ifndef MINIHTTP_THREADPOOL_H
 #define MINIHTTP_THREADPOOL_H
 
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+
 
 class ThreadPool {
+public:
+    using Task = std::function<void()>;
+
+    explicit ThreadPool(size_t thread_count);
+
+    ~ThreadPool();
+
+    void submit(Task task);
+
+    void mStop();
+
+private:
+    void workerLoop();
+
+    std::vector<std::thread> workers_;
+    std::queue<Task> tasks_;
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    bool stop_ = false;
 };
 
 
